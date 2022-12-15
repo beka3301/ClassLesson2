0d367022e300c7e06ad2f77f64a213e8c1abeb5c
beka3301 <bekturhanazarov98@gmail.com>
2022-12-15 21:05:36 +0600

    class

--- src/Main.java
+++ src/Main.java
@@ -10,62 +10,58 @@ public class Main {
         Cat[] cat = new Cat[3];
         Dog[] dog = new Dog[3];
         Horse[] horse = new Horse[3];
-        int countDog= 0;
-
+        int countDog = 0;
+        int pussy = 0;
         while (true) {
             System.out.println("add cat-> 1" + "\n" + "add dog-> 2" + "\n" + "add horse-> 3");
             int temp = in.nextInt();
-        int pussy=0;
-        while (true) {
-        System.out.println("add cat-> 1" + "\n" + "add dog-> 2" + "\n" + "add horse-> 3");
-        int temp = in.nextInt();
-            if (temp==1){
+            if (temp == 1) {
                 for (int i = 0; i < cat.length; i++) {
                     pussy++;
-                System.out.print("name: ");
-                String name = in1.nextLine();
-                System.out.print("color: ");
-                String color= in2.nextLine();
-                System.out.print("age: ");
-                int age= in3.nextInt();
-                cat[i]=cat(name,color,age);
+                    System.out.println("Cat: "+pussy);
+                    System.out.print("name: ");
+                    String name = in1.nextLine();
+                    System.out.print("color: ");
+                    String color = in2.nextLine();
+                    System.out.print("age: ");
+                    int age = in3.nextInt();
+                    cat[i] = cat(name, color, age);
                 }
-                for (Cat cat1:cat) {
+                for (Cat cat1 : cat) {
                     System.out.println(cat1);
                 }
             }
 
-        }
 
             if (temp == 2) {
                 for (int i = 0; i < dog.length; i++) {
                     countDog++;
-                    System.out.println("dog = "+countDog);
+                    System.out.println("dog = " + countDog);
                     System.out.print("name: ");
                     String name = in3.nextLine();
                     System.out.print("color: ");
                     String color = in1.nextLine();
                     System.out.print("age: ");
                     int age = in2.nextInt();
-                    dog[i] = dog(name,color,age);
+                    dog[i] = dog(name, color, age);
                 }
-                for (Dog dog1:dog) {
+                for (Dog dog1 : dog) {
                     System.out.println(dog1);
                 }
             }
 
         }
     }
-    public static Dog dog(String name,String color,int age){
-        return new Dog( name, age, color );
-    }
-}
-        }
-        public static Cat cat(String name,String color,int age){
-        return new Cat(name, age, color);
-        }
 
-        }
+    public static Dog dog(String name, String color, int age) {
+        return new Dog(name, age, color);
+    }
+
+    public static Cat cat(String name, String color, int age) {
+        return new Cat(name, age, color);
+    }
+
+}
 
 
 
