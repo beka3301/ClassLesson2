86097e6b4c292445783c94d1a6b2f082d89a2fac
beka3301 <bekturhanazarov98@gmail.com>
2022-12-15 19:47:00 +0600

    class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-
+        Scanner in= new Scanner(System.in);
+        Cat[] cat = new Cat[3];
+        Dog[] dog = new Dog[3];
+        Horse[] horse = new Horse[3];
+        System.out.println("add cat-> 1" +"\n"+"add dog-> 2"+"\n"+"add horse-> 3");
+        int temp = in.nextInt();
 
     }
 }
